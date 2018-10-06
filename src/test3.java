18c4ad7b2d99f4e4bbeab9727c35948f13d90cf5
USER <USER@DESKTOP-ANN9VSP>
2018-10-06 17:15:09 +0900

    ㅊㅌㅋㅊ

--- src/test3.java
+++ src/test3.java
@@ -3,7 +3,7 @@ public class test3 {
 
 	public static void main(String[] args) {
 		System.out.println("저는 중재입니다.");
-		System.out.println("중재 브랜치 내용을 수정했어요");
+		System.out.println("중재 브랜치 내용을 수정했어1ㄷㅈㅂ요");
 	}
 
 }
